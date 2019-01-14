97f4799e11e55301c326d0914f6e02d3327af5ad
Blaž <blaz.rupnik56@gmail.com>
2019-01-14 11:49:16 +0100

    function for predicting word, still needs some polishing

--- interesting_features.R
+++ interesting_features.R
@@ -57,3 +57,66 @@ library("stringr")
 avg_num_of_sentences = 0
 sapply(strsplit(docs2[[1]]$content, " "), length)
 cat("Average number of sentences: ", avg_num_of_sentences/723)
+
+#findAssocs(dtm2, c("laughter"), corlimit=0.4)
+
+occurences <- list()
+for(j in 1:length(docs2)){
+  text <- docs2[[j]]$content
+  text <- strsplit(text,split=' ')
+  text <- text[[1]]
+  for(i in 1:length(text)){
+    word1 <- text[i]
+    word2 <- text[i+1]
+    occurences[[word1]] <- c(occurences[[word1]], word2)
+  }
+}
+#sorted_table_test <- sort(table(occurences[["people"]]), decreasing = T)
+occurences_after <- list()
+for(j in 1:length(docs2)){
+  text <- docs2[[j]]$content
+  text <- strsplit(text,split = ' ')
+  text <- text[[1]]
+  for(i in 2:length(text)){
+    word1 <- text[i]
+    word2 <- text[i-1]
+    occurences_after[[word1]] <- c(occurences_after[[word1]],word2)
+  }
+}
+
+predictMissingWord <- function(sentence){
+  splitted <- strsplit(sentence, split = '_')[[1]]
+  textBefore = splitted[1]
+  textAfter = splitted[2]
+  hasWordBefore = FALSE
+  hasWordAfter = FALSE
+  if(textBefore != ""){
+    hasWordBefore = TRUE
+  }
+  if(!is.na(textAfter) && textAfter != ""){
+    hasWordAfter = TRUE
+  }
+  wordBefore = ""
+  wordAfter = ""
+  if(hasWordBefore){
+    splittedBefore <- strsplit(textBefore, split = ' ')[[1]]
+    lengthBefore <- length(splittedBefore)
+    wordBefore <- splittedBefore[lengthBefore]
+  }
+  if(hasWordAfter){
+    splittedAfter <- strsplit(textAfter, split = ' ')[[1]]
+    wordAfter <- splittedAfter[1]
+  }
+  missing_word <- ""
+  if(hasWordAfter && hasWordBefore){
+    sorted_table_after <- sort(table(occurences[[wordAfter]]), decreasing = T)
+    #print(names(sorted_table_after))
+    sorted_table_before <- sort(table(occurences_after[[wordBefore]]), decreasing = T)
+    common <- intersect(names(sorted_table_after),names(sorted_table_before))
+    missing_word <- common[1] 
+  }
+  #TODO check if start or end of sentence
+  sentence <- gsub("_",paste("",missing_word,"",sep = " "),sentence)
+  return(sentence)
+}
+
--- vocabulary_predictions.R
+++ vocabulary_predictions.R
@@ -36,11 +36,17 @@ training_set = dataset_2[intrain,]
 test_set = dataset_2[-intrain,]
 View(training_set$Liked)
 library(randomForest)
+library(naivebayes)
+library(e1071)
 classifier = randomForest(x = training_set[-8241], y = training_set$Liked, ntree = 10)
-
+classifier_bayes = naiveBayes(x = training_set[-8241],
+                              type = c("class","raw"),
+                              y = training_set$Liked)
 #prediction
 y_pred = predict(classifier, newdata = test_set[-8241])
-
+y_bayespred = predict(classifier_bayes, newdata = test_set[-8241])
 cm = table(test_set[,8241],y_pred)
 accuracy = sum(diag(cm))/sum(cm)
+cm_bayes = table(test_set[,8241],y_bayespred)
+accuracy_bayes = sum(diag(cm_bayes))/sum(cm_bayes)
 
